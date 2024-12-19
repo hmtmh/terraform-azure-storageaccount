b5c7949c682e0c09650002994c869b524ecfff3b
Adirlik <62341825+hmtmh@users.noreply.github.com>
2024-12-19 13:40:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create a Storage Account
 resource "azurerm_storage_account" "storage" {
-  name                     = "samplestorageacct001" # Must be globally unique
+  name                     = var.storage_account_name # Use variable here
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
